227f4b6fffa1ee4f22fe2210f0907e36a05d4c7e
Marc GLOAGUEN <marc.gloaguen@gmail.com>
2024-01-04 15:56:47 +0100

    MàJ du module de preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -1,9 +1,9 @@
 import pandas as pd
-from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from sklearn.preprocessing import StandardScaler
 
 
 class Preprocessing:
-    def __init__(self, train:pd.DataFrame, test:pd.DataFrame, target:str) :
+    def __init__(self, train: pd.DataFrame, test: pd.DataFrame, target: str):
         self.X = train.drop(columns=target)
         self.test = test
         self.target = train[target]
@@ -13,8 +13,12 @@ class Preprocessing:
         self.test = pd.get_dummies(self.test, columns=['Geography'])
 
     def __gender(self):
-        self.X.loc[:,'Gender'], self.gender_uniques = pd.factorize(self.X.Gender, sort=True)
-        self.test.loc[:,'Gender'] = pd.factorize(self.test.Gender, sort=True)[0]
+        self.X.loc[:, 'Gender'], self.gender_uniques = pd.factorize(
+            self.X.Gender, sort=True
+            )
+        self.test.loc[:, 'Gender'] = pd.factorize(
+            self.test.Gender, sort=True
+            )[0]
 
     def _normal(self):
         scaler = StandardScaler()
