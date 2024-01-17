534c4f43173b972c1162c083bb4d6379963d67a7
Marc GLOAGUEN <marc.gloaguen@gmail.com>
2024-01-17 14:46:40 +0100

    update preprocessing try it kerras

--- preprocessing.py
+++ preprocessing.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from sklearn.preprocessing import StandardScaler, RobustScaler
+from sklearn.preprocessing import StandardScaler, PowerTransformer, MinMaxScaler
 
 
 class Preprocessing:
@@ -10,19 +10,41 @@ class Preprocessing:
         self.data.loc[:, 'Gender'], self.gender_uniques = pd.factorize(self.data.Gender, sort=True)
 
     def __normal_fit_transform(self):
-        self.scaler = RobustScaler()
-        col_norm = ['CreditScore', 'Age', 'Balance', 'Tenure', 'NumOfProducts','EstimatedSalary']
+        self.scaler = StandardScaler()
+        col_norm = ['CreditScore','EstimatedSalary']
         self.data.loc[:, col_norm] = self.scaler.fit_transform(self.data[col_norm])
-
+        
     def __normal_transform(self):
-        col_norm = ['CreditScore', 'Age', 'Balance', 'Tenure', 'NumOfProducts', 'EstimatedSalary']
+        col_norm = ['CreditScore', 'EstimatedSalary']
         self.data.loc[:, col_norm] = self.scaler.transform(self.data[col_norm])
 
+    def __power_fit_transform(self):
+        self.power = PowerTransformer()
+        col_norm = ['Age', 'Balance']
+        self.data.loc[:, col_norm] = self.power.fit_transform(self.data[col_norm])
+    
+    def __power_transform(self):
+        col_norm = ['Age', 'Balance']
+        self.data.loc[:, col_norm] = self.power.transform(self.data[col_norm])
+    
+    def __MinMax_fit_transform(self):
+        self.minmax = MinMaxScaler()
+        col_norm = ['Tenure', 'NumOfProducts']
+        self.data.loc[:, col_norm] = self.minmax.fit_transform(self.data[col_norm])
+    
+    def __MinMax_transform(self):
+        col_norm = ['Tenure', 'NumOfProducts']
+        self.data.loc[:, col_norm] = self.minmax.transform(self.data[col_norm])
+    
+    
+    
     def fit_transform(self, data: pd.DataFrame):
         self.data = data
         self.__geography()
         self.__gender()
         self.__normal_fit_transform()
+        self.__power_fit_transform()
+        self.__MinMax_fit_transform()
         return self.data
 
     def transform(self, data: pd.DataFrame):
@@ -30,4 +52,6 @@ class Preprocessing:
         self.__geography()
         self.__gender()
         self.__normal_transform()
+        self.__power_transform()
+        self.__MinMax_transform()
         return self.data
